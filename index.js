ea4ebe662747d939cf7fb97c33b763e7fe7b5eca
gael <76548719+gaelzarco@users.noreply.github.com>
2023-03-04 17:52:55 -0800

    Testing git pull on Jas' machine

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 console.log('index.js is linked with html')
+
+const cards = {
+    1: {
+        post: {
+            caption: "This is my first post. I am working on CSS styling for my Social Media app.",
+            img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fs3.amazonaws.com%2Fmobileappdaily%2Fmad%2Fuploads%2Fmad_blog_5df2190b8638a1576147211.jpg&f=1&nofb=1&ipt=a8f0c17d303ade0569678d643ec582ce0544c937c2e8834b7610589eba4d5bde&ipo=images",
+            comments: 3,
+            likes: 20,
+            user: {
+                userName: "xarco",
+                firstName: "Gael",
+                lastName: "Zarco",
+                img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.FjSRywv2jSoO2kdlijRZ4gHaHa%26pid%3DApi&f=1&ipt=36cf87b6a2dca56dd72221895855e9daf59f9dbdf29fb7e9a44c727a148afb7d&ipo=images"
+            },
+        }
+    }
+}
